98cc8a4081410026b43995ffdd31d0ec19fca6f3
Michele Condo <mcondokmb@gmail.com>
2025-01-09 11:16:03 +0000

    Added License file. Updated License across projects files.

--- Test.cpp
+++ Test.cpp
@@ -1,3 +1,8 @@
+// Copyright (c) 2018-2025 Michele Condo'
+// File: C:\Projects\Nix\Test.cpp
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+
 // Nix.cpp : Defines the entry point for the console application.
 //
 
